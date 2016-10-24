99725e728bbb65c24c6dc359528607f688e8549d
Marina Boeva <camado6pota@yandex.ru>
2016-10-24 22:53:19 +0300

    submition 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,70 @@
-function getImg(e){var t=new XMLHttpRequest;t.open("GET",e,!1),t.send(null);var r=t.responseText;return r=JSON.parse(r)}function init(e){var t=getImg(e),r=document.querySelector(".grid");r.innerHTML="";for(var i=0;i<7;i++){var a=document.createElement("figure"),n=document.createElement("figcaption");a.setAttribute("class","grid-item"),n.setAttribute("class","title--white"),4!==i&&5!==i||a.setAttribute("class","grid-item grid-item--2"),r.appendChild(a),a.appendChild(n),a.setAttribute("style","background-image:url("+t.hits[i].webformatURL+")"),n.innerHTML=t.hits[i].tags}new Masonry(r,{containerStyle:null,columnWidth:".grid-item",itemSelector:".grid-item",gutter:10})}function getNewQuery(){var e=document.querySelector(".ideas__input"),t=e.value;null!=t&&init("https://pixabay.com/api/?key=3429450-a383cae33a92ccea1a1e14e3a&&image_type=photo&pretty=true&per_page=7&min_height=310&min_width=600&orientation=horisontal&page=2&q="+t),e.value=""}if(!XMLHttpRequest)var XMLHttpRequest=function(){for(var e=["Msxml2.XMLHTTP.3.0","Msxml2.XMLHTTP","Microsoft.XMLHTTP"],t=0,r=e.length;t<r;t++)try{return new ActiveXObject(e[t])}catch(e){}return null};$(function(){$(".slide__hider").jcarousel({scroll:1,wrap:"circular",list:".slide__container"}),$(".slide__control--left").jcarouselControl({target:"-=1"}),$(".slide__control--right").jcarouselControl({target:"+=1"}),$(".ideas__button").on("click",function(e){e.preventDefault(),getNewQuery()});var e=["sunset","mountain","holland","fjords","venice+canal","ireland+lake","alps+nature"];category=e[Math.floor(Math.random()*e.length)],init("https://pixabay.com/api/?key=3429450-a383cae33a92ccea1a1e14e3a&&image_type=photo&pretty=true&per_page=7&min_height=300&min_width=610&orientation=horisontal&page=1&q="+category)});
+  if (!XMLHttpRequest){
+      var XMLHttpRequest = function() {
+      	for (var progIDs = ['Msxml2.XMLHTTP.3.0', 'Msxml2.XMLHTTP', 'Microsoft.XMLHTTP'],
+      		i = 0, length = progIDs.length; i < length; i++) try {
+      			return new ActiveXObject(progIDs[i]);
+      		} catch (err) {}
+      	return null;
+      };
+    }
+  function getImg(q) {
+    var req = new XMLHttpRequest();
+    req.open('GET',q,false);
+    req.send(null);
+    var data= req.responseText;
+    data = JSON.parse(data);
+    return data;
+  }
+  function init(q) {
+    var data=getImg(q);
+    var container = document.querySelector('.grid');
+    container.innerHTML="";
+    for (var i = 0; i <7; i++) {
+      var figure=document.createElement('figure');
+      var figcaption=document.createElement('figcaption');
+      figure.setAttribute('class','grid-item');
+      figcaption.setAttribute('class','title title--white');
+        if(i===4||i===5){
+          figure.setAttribute('class','grid-item grid-item--2');
+      }
+      container.appendChild(figure);
+      figure.appendChild(figcaption);
+      figure.setAttribute('style','background-image:url('+data.hits[i].webformatURL+')');
+      figcaption.innerHTML=data.hits[i].tags;
+    }
+    var msnry = new Masonry( container, {
+      containerStyle: null,
+      columnWidth: ".grid-item",
+      itemSelector: '.grid-item',
+      gutter:12
+    });
+ }
+$(function () {
+  $('.slide__hider').jcarousel({
+    scroll:1,
+    wrap:"circular",
+    list: '.slide__container'
+  });
+  $('.slide__control--left').jcarouselControl({
+        target: '-=1'
+    });
+
+    $('.slide__control--right').jcarouselControl({
+        target: '+=1'
+    });
+  $('.ideas__button').on('click',function (event) {
+    event.preventDefault();
+    getNewQuery();
+  });
+  var categorys=['sunset','seaside','mountain','holland','fjords','venice+canal','ireland+lake','alps+nature'];
+  category=categorys[Math.floor(Math.random()*categorys.length)];
+  init('https://pixabay.com/api/?key=3429450-a383cae33a92ccea1a1e14e3a&&image_type=photo&pretty=true&per_page=7&min_height=300&min_width=600&orientation=horisontal&page=3&q='+category);
+});
+function getNewQuery() {
+  var input = document.querySelector('.ideas__input');
+  var keyword =input.value;
+  if(keyword!=null){
+    init('https://pixabay.com/api/?key=3429450-a383cae33a92ccea1a1e14e3a&&image_type=photo&pretty=true&per_page=7&min_height=300&min_width=600&orientation=horisontal&page=2&q='+keyword);
+  }
+  input.value=''
+}
